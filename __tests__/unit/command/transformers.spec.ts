4d0be1ddb63d4ecf34cb0ae63eef95a4a0ba59bd
Guðjón Jónsson <gudjonj@gmail.com>
2018-12-31 04:32:40 +0000

    Added transformer tests

--- __tests__/unit/command/transformers.spec.ts
+++ __tests__/unit/command/transformers.spec.ts
@@ -1,4 +1,59 @@
+import * as transformers from '../../../src/transformer';
 import * as mock from '../../__mocks__/basic';
+
+describe('transformers', () => {
+  test('it should transform on first valid transformer (2nd)', () => {
+    const t = new transformers.Transformer([
+      {
+        predicate: (val: any) => val.indexOf('_') !== -1,
+        replacer: (val: any) => `-${val}-`
+      },
+      {
+        predicate: (val: any) => val.indexOf('_') === -1,
+        replacer: (val: any) => `_${val}_`
+      }
+    ]);
+
+    const v = t.first('foo');
+
+    expect(v).toBe('_foo_');
+  });
+
+  test('it should transform on all valid transformers', () => {
+    const t = new transformers.Transformer([
+      {
+        predicate: () => true,
+        replacer: (val: any) => `-${val}-`
+      },
+      {
+        predicate: () => true,
+        replacer: (val: any) => `_${val}_`
+      }
+    ]);
+
+    const v = t.all('foo');
+
+    expect(v).toBe('_-foo-_');
+  });
+
+  test('it should transform on a single named transformer', () => {
+    const t = new transformers.Transformer([
+      {
+        predicate: () => true,
+        replacer: (val: any) => `-${val}-`
+      },
+      {
+        name: 'double',
+        predicate: () => true,
+        replacer: (val: any) => `${val}${val}`
+      }
+    ]);
+
+    const v = t.single('foo', 'double');
+
+    expect(v).toBe('foofoo');
+  });
+});
 
 describe('creating commands with transformers enabled', () => {
   test('it should return a long command with quotes', () => {
