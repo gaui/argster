bbf61d6c26e5b84753f8ed8a95d914619adb4adf
Gudjon Jonsson <gudjonj@gmail.com>
2019-03-31 03:44:26 +0000

    Migrated from TSLint to ESLint

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: './tsconfig.json'
+  },
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+    'prettier',
+    'prettier/@typescript-eslint'
+  ],
+  plugins: ['@typescript-eslint'],
+  rules: {
+    '@typescript-eslint/interface-name-prefix': 'off',
+    '@typescript-eslint/no-use-before-define': ['error', 'nofunc']
+  }
+};
