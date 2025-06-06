065f0487513ffa632a2950dcc3cc7e534e160a2d
Gudjon <gudjonj@gmail.com>
2025-06-06 15:21:06 +0000

    fix(fileutils): support multiple glob patterns

---
+++ __tests__/unit/fileUtils.spec.ts
@@ -0,0 +1,19 @@
+import path from 'path';
+import { FileUtils } from '../../src/utils';
+
+describe('searchFilesForPatterns', () => {
+  test('it should find files for multiple patterns', () => {
+    const utils = new FileUtils();
+    const rootDir = path.join(__dirname, '../e2e/data');
+    const patterns = ['**/*.env', '**/*.vol'];
+    const files = utils.searchFilesForPatterns(patterns, rootDir);
+
+    const expected = [
+      path.join(rootDir, 'test.env'),
+      path.join(rootDir, 'test.vol')
+    ];
+
+    expect(files).toEqual(expect.arrayContaining(expected));
+    expect(files.length).toBe(expected.length);
+  });
+});
