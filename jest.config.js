801497f8cb37d4d6caa5e444829edc3ad529c736
Gudjon Jonsson <gudjonj@gmail.com>
2025-06-06 18:26:58 +0000

    chore: removed babel and use ts-jest

--- jest.config.js
+++ jest.config.js
@@ -2,11 +2,8 @@
 // https://jestjs.io/docs/en/configuration.html
 
 module.exports = {
+  preset: 'ts-jest',
   testEnvironment: 'node',
-
-  transform: {
-    '^.+\\.(ts|tsx)$': 'babel-jest'
-  },
   coverageDirectory: '.coverage',
-  testPathIgnorePatterns: ['/node_modules/', '/lib/', '/__mocks__/']
+  testPathIgnorePatterns: ['/node_modules/', '/lib/', '/__mocks__/'],
 };
