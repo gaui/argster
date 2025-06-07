e52dec3ba765491e3cb0348f9e9f44d05af2defd
Gudjon <gudjonj@gmail.com>
2025-06-07 00:11:40 +0000

    chore(tests): use import.meta.dirname

--- __tests__/unit/fileUtils.spec.ts
+++ __tests__/unit/fileUtils.spec.ts
@@ -4,7 +4,7 @@ import { FileUtils } from '../../src/utils';
 describe('searchFilesForPatterns', () => {
   test('it should find files for multiple patterns', () => {
     const utils = new FileUtils();
-    const rootDir = path.relative(process.cwd(), path.join(__dirname, '../e2e/data'));
+    const rootDir = path.join(import.meta.dirname, '../e2e/data');
     const patterns = ['**/*.env', '**/*.vol'];
     const files = utils.searchFilesForPatterns(patterns, rootDir);
 
@@ -15,5 +15,43 @@ describe('searchFilesForPatterns', () => {
 
     expect(files).toEqual(expect.arrayContaining(expected));
     expect(files.length).toBe(expected.length);
+  });
+
+  test('it should find .env and .vol files', () => {
+    const utils = new FileUtils();
+    const rootDir = path.join(import.meta.dirname, '../e2e/data');
+
+    const envFiles = utils.searchFilesForPatterns(['**/*.env'], rootDir);
+    const volFiles = utils.searchFilesForPatterns(['**/*.vol'], rootDir);
+
+    expect(envFiles).toEqual([path.join(rootDir, 'test.env')]);
+    expect(volFiles).toEqual([path.join(rootDir, 'test.vol')]);
+  });
+
+  test('it should return unique results for overlapping patterns', () => {
+    const utils = new FileUtils();
+    const rootDir = path.join(import.meta.dirname, '../e2e/data');
+
+    const patterns = ['**/*.*', '**/*.env'];
+    const files = utils.searchFilesForPatterns(patterns, rootDir);
+
+    const expected = [
+      path.join(rootDir, 'test.vol'),
+      path.join(rootDir, 'test.lbl'),
+      path.join(rootDir, 'test.env')
+    ];
+
+    expect(files).toEqual(expected);
+  });
+});
+
+describe('computeFileContents', () => {
+  test('it handles empty patterns', () => {
+    const utils = new FileUtils();
+    const patterns: IArgumentFilePatterns[] = [{ prefix: '--env', patterns: [] }];
+
+    const results = utils.computeFileContents(patterns);
+
+    expect(results[0].contents).toEqual([]);
   });
 });
